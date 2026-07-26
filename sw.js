6b18da0de874be36761da35352f343e791545fec
Rami-Alyazji <sstopinhere@gmail.com>
2026-07-26 22:40:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,33 @@
-const CACHE_NAME = "ghars-app-v2";
-const urlsToCache = [
-  "./index.html",
-  "./logo.jpg",
-  "./manifest.json"
+const CACHE_NAME = "ghars-app-v4";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./logo.jpg"
 ];
 
-// تثبيت ملفات التطبيق في ذاكرة الهاتف
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", (e) => {
+    self.skipWaiting(); // هذا السطر السحري يجبر التطبيق على التحديث فوراً دون انتظار
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
 });
 
-// جلب الملفات من الذاكرة عند انقطاع الإنترنت
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            );
+        })
+    );
+    return self.clients.claim(); // وهذا السطر يطبق التحديث فورا على الشاشة
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => {
+            return res || fetch(e.request);
+        })
+    );
 });
